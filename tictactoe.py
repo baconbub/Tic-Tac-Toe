2b0aa45deddd1aacbc768f8d1693c189d9509338
baconbub <zacharycoe94@gmail.com>
2024-10-16 04:35:16 -0500

    Initializing repo

---
+++ tictactoe.py
@@ -0,0 +1,38 @@
+"""
+Game Name: Tic-Tac-Toe
+Description: Play against a bot in Tic-Tac-Toe
+Author: Zachary Coe
+Date: 2024-10-16
+"""
+
+
+
+import random
+
+
+
+class Board:
+    def __init__(self):
+        self.positions = [None] * 9
+        self.appearance = """
+   |   |   
+---+---+---
+   |   |   
+---+---+---
+   |   |   """
+        
+class Player:
+    pass
+
+class Game:
+    pass
+
+
+
+def main():
+    return
+
+
+
+if __name__ == "__main__":
+    main()
